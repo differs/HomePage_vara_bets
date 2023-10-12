e1e12c5e1a31dd92987fff6a8953d095bfeb7bf7
differs <keepmysterious@tutanota.com>
2023-10-12 16:54:25 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+        <p>          
+          VARA BETS IS A FAIR AND JUST GAMBLING GAME 
+          
+        </p>
+        <p>
+        BUILT WITH BLOCKCHAIN TECHNOLOGE 
+        </p>
+        <p>
+          ON VARA CHAIN
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
