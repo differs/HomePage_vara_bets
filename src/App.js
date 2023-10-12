ccc3496274106d6b33cd31637d4134dc79492a52
differs <keepmysterious@tutanota.com>
2023-10-12 17:02:25 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
